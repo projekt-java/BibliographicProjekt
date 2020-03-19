bef070ce06c16b014dfe11493dc215e6766bdcff
borchowiec <borchowiecpatryk@gmail.com>
2020-03-19 16:19:06 +0100

    Added path argument to write method in BooksWriter.

--- src/main/java/com/pk/writer/BooksWriter.java
+++ src/main/java/com/pk/writer/BooksWriter.java
@@ -9,6 +9,7 @@ public interface BooksWriter {
     /**
      * Writes given books to file.
      * @param books Books that will be written to file.
+     * @param path Path of final file.
      */
-    void write(Book[] books);
+    void write(Book[] books, String path);
 }
