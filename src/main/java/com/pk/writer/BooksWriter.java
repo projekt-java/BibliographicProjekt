407b32f333e02e18d565217efd76838d7db324cb
borchowiec <borchowiecpatryk@gmail.com>
2020-03-19 16:17:41 +0100

    Added documentation.

--- src/main/java/com/pk/writer/BooksWriter.java
+++ src/main/java/com/pk/writer/BooksWriter.java
@@ -2,6 +2,13 @@ package com.pk.writer;
 
 import com.pk.model.Book;
 
+/**
+ * Writes books to files.
+ */
 public interface BooksWriter {
+    /**
+     * Writes given books to file.
+     * @param books Books that will be written to file.
+     */
     void write(Book[] books);
 }
--- src/main/java/com/pk/writer/BooksWriterFactory.java
+++ src/main/java/com/pk/writer/BooksWriterFactory.java
@@ -1,10 +1,23 @@
 package com.pk.writer;
 
+/**
+ * Returns specific {@link BooksWriter}.
+ */
 public class BooksWriterFactory {
     public static final int BIBTEX = 0;
     public static final int RTF = 1;
     public static final int TXT = 2;
 
+    /**
+     * <p>Returns {@link BooksWriter}. You can choose specific writer by giving one of the constants that represents
+     * format of final file:</p>
+     * <p>{@link #BIBTEX}</p>
+     * <p>{@link #RTF}</p>
+     * <p>{@link #TXT}</p>
+     * <p>If you give wrong argument {@link IllegalArgumentException} will be thrown.</p>
+     * @param format Format of final file.
+     * @return Specific {@link BooksWriter}.
+     */
     public static BooksWriter getBooksWriter(int format) {
         switch (format) {
             case BIBTEX:
