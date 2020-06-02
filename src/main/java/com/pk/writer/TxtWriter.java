ebd697fd5f49810c7a87cb712ee58ac02542c297
bfilipczyk <bfilipczyk119@gmail.com>
2020-06-02 13:05:39 +0200

    Added write method to TxtWriter class

--- src/main/java/com/pk/writer/TxtWriter.java
+++ src/main/java/com/pk/writer/TxtWriter.java
@@ -2,9 +2,26 @@ package com.pk.writer;
 
 import com.pk.model.Book;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 public class TxtWriter implements BooksWriter {
     @Override
     public void write(Book[] books, String path){
+
+        try(
+                FileWriter file = new FileWriter(path);
+                BufferedWriter writer = new BufferedWriter(file)
+        ) {
+                for ( Book book: books){
+                    writer.write(book.toString()+"\n");
+                }
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
 
     }
 }
