3a84ce54dd1958c7a67d64490aa3e1590f3aabfd
Jacob Chwastek <jacobchwastek@gmail.com>
2020-06-05 19:09:24 +0200

    Rtf code refactor

--- src/main/java/com/pk/writer/RtfWriter.java
+++ src/main/java/com/pk/writer/RtfWriter.java
@@ -2,34 +2,36 @@ package com.pk.writer;
 
 import com.pk.model.Book;
 import com.tutego.jrtf.Rtf;
+import com.tutego.jrtf.RtfPara;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
-import static com.tutego.jrtf.RtfDocfmt.*;
-import static com.tutego.jrtf.RtfInfo.*;
-import static com.tutego.jrtf.RtfPara.*;
-import static com.tutego.jrtf.RtfUnit.*;
+import static com.tutego.jrtf.RtfDocfmt.defaultTab;
+import static com.tutego.jrtf.RtfDocfmt.revisionMarking;
+import static com.tutego.jrtf.RtfPara.p;
+import static com.tutego.jrtf.RtfUnit.CM;
 
 public class RtfWriter implements BooksWriter {
     @Override
     public void write(Book[] books, String path) {
-        try
-        {
-            /* TODO: We need to discuss what rtf styling we would like to implement */
-            //  https://github.com/ullenboom/jrtf
-            var temp = Rtf.rtf();
+        try {
+            List<RtfPara> paras = new LinkedList<>();
             for (Book book :books) {
-                        temp.info(author(book.getAuthor()), title(book.getTitle()))
-                        .documentFormatting(defaultTab(1, CM), revisionMarking()).section(
-                                p(book.getTitle()),
-                                p(book.getGenre()),
-                                p(book.getPublisher()),
-                                p(book.getPublishedDate()),
-                                p(book.getPrice()),
-                                p(book.getDescription()));
+                paras.add(p(book.getAuthor()));
+                paras.add(p(book.getTitle()));
+                paras.add(p(book.getGenre()));
+                paras.add(p(book.getPublisher()));
+                paras.add(p(book.getPublishedDate()));
+                paras.add(p(book.getPrice()));
+                paras.add(p(book.getDescription()));
+                paras.add(p(""));
             }
-            temp.out(new FileWriter(path));
-
+            Rtf rtf = Rtf.rtf();
+            rtf.documentFormatting(defaultTab(1, CM), revisionMarking()).section(paras);
+            rtf.out(new FileWriter(path));
         }
         catch (IOException exception) {
             exception.printStackTrace();
