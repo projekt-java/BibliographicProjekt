40d0b527952be2c08665dc9ec888186fff76a3b7
borchowiec <borchowiecpatryk@gmail.com>
2020-06-13 22:43:38 +0200

    Fixed test directory structure. Added surefire plugin.

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.pk {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires dom4j;
-    requires jrtf;
-    exports com.pk;
-    exports com.pk.controller;
-    exports com.pk.model;
-}
